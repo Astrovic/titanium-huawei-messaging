25aeeacc75bea3518729d3553113616e04ae624b
hansemannn <hans.knoechel@hs-osnabrueck.de>
2020-07-07 21:10:52 +0200

    chore: expose new APIs

--- android/src/huawei/messaging/TitaniumHuaweiMessagingModule.kt
+++ android/src/huawei/messaging/TitaniumHuaweiMessagingModule.kt
@@ -3,11 +3,14 @@ package huawei.messaging
 import com.huawei.agconnect.appmessaging.AGConnectAppMessaging
 import com.huawei.agconnect.appmessaging.AGConnectAppMessagingOnClickListener
 import com.huawei.agconnect.appmessaging.model.AppMessage
+import com.huawei.agconnect.config.AGConnectServicesConfig
+import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.push.HmsMessaging
 import org.appcelerator.kroll.KrollDict
 import org.appcelerator.kroll.KrollModule
-import org.appcelerator.kroll.annotations.Kroll.method
-import org.appcelerator.kroll.annotations.Kroll.module
+import org.appcelerator.kroll.annotations.Kroll.*
 import org.appcelerator.kroll.common.Log
+import org.appcelerator.titanium.TiApplication
 
 
 class ClickListener : AGConnectAppMessagingOnClickListener {
@@ -35,6 +38,56 @@ class TitaniumHuaweiMessagingModule : KrollModule() {
     @method
     fun trigger(event: String) {
         instance.trigger(event)
+    }
+
+    @method
+    fun subscribe(topic: String) {
+        HmsMessaging.getInstance(TiApplication.getAppCurrentActivity())
+                .subscribe(topic)
+                .addOnCompleteListener { task ->
+                    val event = KrollDict()
+                    event["success"] = task.isSuccessful
+                    fireEvent("subscribe", event)
+                }
+    }
+
+    @method
+    fun unsubscribe(topic: String) {
+        HmsMessaging.getInstance(TiApplication.getAppCurrentActivity())
+                .unsubscribe(topic)
+                .addOnCompleteListener { task ->
+                    val event = KrollDict()
+                    event["success"] = task.isSuccessful
+                    fireEvent("subscribe", event)
+                }
+    }
+
+    @setProperty
+    fun enablePush(enabled: Boolean) {
+        val messagingInstance = HmsMessaging.getInstance(TiApplication.getAppCurrentActivity())
+
+        if (enabled) {
+            messagingInstance.turnOnPush().addOnCompleteListener { _ ->
+                // Handle task state?
+            }
+        } else {
+            messagingInstance.turnOffPush().addOnCompleteListener { _ ->
+                // Handle task state?
+            }
+        }
+    }
+
+    @getProperty
+    @method
+    fun getToken(): String {
+        val appId = AGConnectServicesConfig.fromContext(TiApplication.getAppCurrentActivity()).getString("client/app_id")
+        return HmsInstanceId.getInstance(TiApplication.getAppCurrentActivity()).getToken(appId, "HCM")
+    }
+
+    @method
+    fun deleteToken() {
+        val appId = AGConnectServicesConfig.fromContext(TiApplication.getAppCurrentActivity()).getString("client/app_id")
+        HmsInstanceId.getInstance(TiApplication.getAppCurrentActivity()).deleteToken(appId, "HCM")
     }
 
     fun onTokenRefresh(token: String) {
